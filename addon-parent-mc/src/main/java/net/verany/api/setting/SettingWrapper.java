9452247288671833d75e1b98698d610bef89b886
tylix <bytydoo@gmail.com>
2021-02-04 14:39:56 +0100

    Update SettingWrapper.java
    - added material to settings

--- addon-parent-mc/src/main/java/net/verany/api/setting/SettingWrapper.java
+++ addon-parent-mc/src/main/java/net/verany/api/setting/SettingWrapper.java
@@ -1,16 +1,16 @@
 package net.verany.api.setting;
 
 import net.verany.api.settings.AbstractSetting;
-import net.verany.api.settings.Settings;
+import org.bukkit.Material;
 
 public class SettingWrapper<T> extends AbstractSetting<T> {
-    public SettingWrapper(String key, String category, Class<T> tClass, T defaultValue) {
-        super(key, category, tClass, defaultValue);
+    public SettingWrapper(String key, String category, Class<T> tClass, T defaultValue, Material material) {
+        super(key, category, tClass, defaultValue, material);
         Settings.VALUES.add(this);
     }
 
-    public SettingWrapper(String key, String category, Class<T> tClass, T defaultValue, boolean inInventory) {
-        super(key, category, tClass, defaultValue, inInventory);
+    public SettingWrapper(String key, String category, Class<T> tClass, T defaultValue, Material material, boolean inInventory) {
+        super(key, category, tClass, defaultValue, material, inInventory);
         Settings.VALUES.add(this);
     }
 }
