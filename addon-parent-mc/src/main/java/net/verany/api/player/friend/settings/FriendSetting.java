3fcba09a2d280fb3023672cd2b189aac2c32c9c9
tylix <bytydoo@gmail.com>
2021-02-04 14:41:01 +0100

    Update FriendSetting.java
    - added material

--- addon-parent-mc/src/main/java/net/verany/api/player/friend/settings/FriendSetting.java
+++ addon-parent-mc/src/main/java/net/verany/api/player/friend/settings/FriendSetting.java
@@ -2,9 +2,10 @@ package net.verany.api.player.friend.settings;
 
 import net.verany.api.setting.SettingWrapper;
 import net.verany.api.settings.AbstractSetting;
+import org.bukkit.Material;
 
 public class FriendSetting {
 
-    public static final AbstractSetting<Boolean> REQUESTS = new SettingWrapper<>("friend_requests", "friends", Boolean.class, true, true);
+    public static final AbstractSetting<Boolean> REQUESTS = new SettingWrapper<>("friend_requests", "FRIENDS", Boolean.class, true, Material.WRITABLE_BOOK, true);
 
 }
