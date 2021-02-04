7e3d21232cf8b74cbac8a7b1647309067c14680e
tylix <bytydoo@gmail.com>
2021-02-04 14:41:49 +0100

    Update AbstractGroupTime.java

--- addon-api-mc/src/main/java/net/verany/api/player/permission/duration/AbstractGroupTime.java
+++ addon-api-mc/src/main/java/net/verany/api/player/permission/duration/AbstractGroupTime.java
@@ -2,21 +2,26 @@ package net.verany.api.player.permission.duration;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import net.verany.api.AbstractVerany;
 
 import java.util.concurrent.TimeUnit;
 
 @AllArgsConstructor
+@Getter
 public abstract class AbstractGroupTime {
     private final TimeUnit timeUnit;
+    private final String key;
 
     public GroupDuration of(int amount) {
-        return new GroupDuration(timeUnit.toMillis(amount));
+        return new GroupDuration(amount, timeUnit.toMillis(amount), key);
     }
 
     @AllArgsConstructor
     @Getter
     public static class GroupDuration {
+        private final int amount;
         private final long millis;
+        private final String key;
     }
 
 }
