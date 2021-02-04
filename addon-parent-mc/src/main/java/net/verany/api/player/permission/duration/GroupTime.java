de92db6240c4fd80be2f26f2344bef29d56acde8
tylix <bytydoo@gmail.com>
2021-02-04 14:40:51 +0100

    Update GroupTime.java
    - added key to time

--- addon-parent-mc/src/main/java/net/verany/api/player/permission/duration/GroupTime.java
+++ addon-parent-mc/src/main/java/net/verany/api/player/permission/duration/GroupTime.java
@@ -4,12 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 public class GroupTime extends AbstractGroupTime {
 
-    public static final AbstractGroupTime HOURS = new GroupTime(TimeUnit.HOURS);
-    public static final AbstractGroupTime DAYS = new GroupTime(TimeUnit.DAYS);
-    public static final GroupDuration LIFETIME = new GroupDuration(-1);
+    public static final AbstractGroupTime HOURS = new GroupTime(TimeUnit.HOURS, "Hours");
+    public static final AbstractGroupTime DAYS = new GroupTime(TimeUnit.DAYS, "days");
+    public static final GroupDuration LIFETIME = new GroupDuration(-1, -1, "lifetime");
 
-    public GroupTime(TimeUnit timeUnit) {
-        super(timeUnit);
+    public GroupTime(TimeUnit timeUnit, String key) {
+        super(timeUnit, key);
     }
-
 }
