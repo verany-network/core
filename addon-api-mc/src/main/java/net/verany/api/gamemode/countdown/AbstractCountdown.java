4caea297a75833fda7e59a13f3b45510ca7a95bf
tylix <bytydoo@gmail.com>
2021-02-09 22:29:15 +0100

    Create AbstractCountdown.java

---
+++ addon-api-mc/src/main/java/net/verany/api/gamemode/countdown/AbstractCountdown.java
@@ -0,0 +1,44 @@
+package net.verany.api.gamemode.countdown;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public abstract class AbstractCountdown {
+
+    private final String key;
+    private final int seconds;
+    private final Integer[] run;
+    private int count;
+    private boolean running = false;
+
+    protected AbstractCountdown(String key, int seconds, Integer... run) {
+        this.key = key;
+        this.seconds = seconds;
+        this.run = run;
+        this.count = seconds;
+    }
+
+    public abstract void run();
+
+    public void finished() {
+    }
+
+    public void onCount() {
+    }
+
+    public void start() {
+        running = true;
+    }
+
+    public void pause() {
+        running = false;
+    }
+
+    public void stop() {
+        running = false;
+        count = seconds;
+    }
+
+}
