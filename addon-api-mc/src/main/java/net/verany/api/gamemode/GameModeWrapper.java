d4d1cf7fb3ab958a6adec57e3131103a49aae210
tylix <bytydoo@gmail.com>
2021-02-09 22:29:37 +0100

    Update GameModeWrapper.java
    - added list of gamemodes

--- addon-api-mc/src/main/java/net/verany/api/gamemode/GameModeWrapper.java
+++ addon-api-mc/src/main/java/net/verany/api/gamemode/GameModeWrapper.java
@@ -1,8 +1,14 @@
 package net.verany.api.gamemode;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
 public class GameModeWrapper extends AbstractGameMode {
+
+    public static List<AbstractGameMode> VALUES = new CopyOnWriteArrayList<>();
 
     public GameModeWrapper(String name, String[] targetGroups) {
         super(name, targetGroups);
+        VALUES.add(this);
     }
 }
