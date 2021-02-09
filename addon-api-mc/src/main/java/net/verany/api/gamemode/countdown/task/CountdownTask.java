b8f2663a794ff952a67c22d3474511aa50347f50
tylix <bytydoo@gmail.com>
2021-02-09 22:29:17 +0100

    Create CountdownTask.java

---
+++ addon-api-mc/src/main/java/net/verany/api/gamemode/countdown/task/CountdownTask.java
@@ -0,0 +1,29 @@
+package net.verany.api.gamemode.countdown.task;
+
+import net.verany.api.AbstractVerany;
+import net.verany.api.gamemode.countdown.AbstractCountdown;
+import net.verany.api.task.AbstractTask;
+
+import java.util.Arrays;
+
+public class CountdownTask extends AbstractTask {
+
+    public CountdownTask(long waitTime) {
+        super(waitTime);
+    }
+
+    @Override
+    public void run() {
+        for (AbstractCountdown countdown : AbstractVerany.COUNTDOWNS) {
+            if (!countdown.isRunning()) continue;
+            if (Arrays.asList(countdown.getRun()).contains(countdown.getCount()))
+                countdown.run();
+            countdown.setCount(countdown.getCount() - 1);
+            countdown.onCount();
+            if (countdown.getCount() == 0) {
+                countdown.finished();
+                AbstractVerany.COUNTDOWNS.remove(countdown);
+            }
+        }
+    }
+}
