27fc2f8e5fa70b88aad3aeadad03f3f82a601fca
tylix <bytydoo@gmail.com>
2021-02-09 22:29:46 +0100

    Update VeranyGameMode.java

--- addon-api-mc/src/main/java/net/verany/api/gamemode/VeranyGameMode.java
+++ addon-api-mc/src/main/java/net/verany/api/gamemode/VeranyGameMode.java
@@ -4,5 +4,14 @@ public class VeranyGameMode {
 
     public static final AbstractGameMode FLAG_WARS = new GameModeWrapper("FlagWars", new String[]{"FW"});
     public static final AbstractGameMode HUB = new GameModeWrapper("Hub", new String[]{"Hub"});
+    public static final AbstractGameMode VIP_HUB = new GameModeWrapper("VIP-Hub", new String[]{"VIP-Hub"});
+    public static final AbstractGameMode ARCADE = new GameModeWrapper("Arcade", new String[]{"Bingo"});
+
+    public static AbstractGameMode getGameModeByName(String name) {
+        for (AbstractGameMode value : GameModeWrapper.VALUES)
+            if(value.getName().equalsIgnoreCase(name))
+                return value;
+        return null;
+    }
 
 }
