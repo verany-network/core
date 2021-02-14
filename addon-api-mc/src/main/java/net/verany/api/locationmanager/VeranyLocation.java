57a61885695b6e0805a07a087ea10c4502f0a71b
tylix <bytydoo@gmail.com>
2021-02-14 21:21:10 +0100

    Update VeranyLocation.java

--- addon-api-mc/src/main/java/net/verany/api/locationmanager/VeranyLocation.java
+++ addon-api-mc/src/main/java/net/verany/api/locationmanager/VeranyLocation.java
@@ -17,6 +17,10 @@ public class VeranyLocation {
         return new Location(Bukkit.getWorld(world), x, y, z, yaw, pitch);
     }
 
+    public static VeranyLocation emptyLocation() {
+        return new VeranyLocation("-", -1, -1, -1, -1, -1);
+    }
+
     public static VeranyLocation toVeranyLocation(Location location) {
         return new VeranyLocation(location.getWorld().getName(), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
     }
