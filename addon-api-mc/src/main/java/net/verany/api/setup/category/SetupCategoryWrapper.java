b3c15980e7afe9e367ae6ae5b0aa629a21a40f45
tylix <bytydoo@gmail.com>
2021-02-14 21:21:42 +0100

    Create SetupCategoryWrapper.java

---
+++ addon-api-mc/src/main/java/net/verany/api/setup/category/SetupCategoryWrapper.java
@@ -0,0 +1,10 @@
+package net.verany.api.setup.category;
+
+import org.bukkit.Material;
+
+public class SetupCategoryWrapper extends AbstractSetupCategory {
+
+    public SetupCategoryWrapper(Material material) {
+        super(material);
+    }
+}
