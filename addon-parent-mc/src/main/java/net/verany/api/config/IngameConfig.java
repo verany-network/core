5181871a769de6aeea31789aea2bbfaa3fe1dee0
tylix <bytydoo@gmail.com>
2021-02-16 22:36:22 +0100

    Update IngameConfig.java

--- addon-parent-mc/src/main/java/net/verany/api/config/IngameConfig.java
+++ addon-parent-mc/src/main/java/net/verany/api/config/IngameConfig.java
@@ -1,9 +1,5 @@
 package net.verany.api.config;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import net.verany.api.game.GameState;
-
 import java.util.concurrent.TimeUnit;
 
 public class IngameConfig<T> extends AbstractIngameConfig<T> {
@@ -16,7 +12,6 @@ public class IngameConfig<T> extends AbstractIngameConfig<T> {
     public static final AbstractIngameConfig<Boolean> PLAYER_COLLISION = new IngameConfig<>(Boolean.class, true);
     public static final AbstractIngameConfig<Long> AFK_TIME = new IngameConfig<>(Long.class, TimeUnit.MINUTES.toMillis(5));
     public static final AbstractIngameConfig<Boolean> AFK = new IngameConfig<>(Boolean.class, false);
-    public static final AbstractIngameConfig<GameState> GAME_STATE = new IngameConfig<>(GameState.class, GameState.LOBBY);
 
     public IngameConfig(Class<T> tClass, T defaultValue) {
         super(tClass, defaultValue);
