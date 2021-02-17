3c1a5f24a355f6d4ec6a780e7fad8e6d002b70e2
tylix <bytydoo@gmail.com>
2021-02-17 13:26:14 +0100

    Update FriendSetting.java

--- addon-parent-mc/src/main/java/net/verany/api/player/friend/settings/FriendSetting.java
+++ addon-parent-mc/src/main/java/net/verany/api/player/friend/settings/FriendSetting.java
@@ -7,5 +7,6 @@ import org.bukkit.Material;
 public class FriendSetting {
 
     public static final AbstractSetting<Boolean> REQUESTS = new SettingWrapper<>("friend_requests", "FRIENDS", Boolean.class, true, Material.WRITABLE_BOOK, true);
+    public static final AbstractSetting<Boolean> JOIN_QUIT_MESSAGES = new SettingWrapper<>("join_quit_messages", "FRIENDS", Boolean.class, true, Material.PAPER, true);
 
 }
