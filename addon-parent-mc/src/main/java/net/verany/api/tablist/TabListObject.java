68fdf0c596812dcf269cf88a51c41d3d01e0f3d2
tylix <bytydoo@gmail.com>
2021-03-03 23:59:37 +0100

    Update TabListObject.java

--- addon-parent-mc/src/main/java/net/verany/api/tablist/TabListObject.java
+++ addon-parent-mc/src/main/java/net/verany/api/tablist/TabListObject.java
@@ -6,6 +6,8 @@ import net.verany.api.player.IPlayerInfo;
 import net.verany.api.player.permission.IPermissionObject;
 import net.verany.api.player.permission.group.AbstractPermissionGroup;
 import net.verany.api.player.permission.group.PermissionGroup;
+import net.verany.volcano.GameSetting;
+import net.verany.volcano.player.IVolcanoPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -22,18 +24,9 @@ public class TabListObject implements ITablistObject {
 
     @Override
     public void setTabList(Player player) {
-        if (!IngameConfig.TAB_LIST.getValue()) return;
+        if (Verany.getPlayer(player.getUniqueId()).getPlayer(IVolcanoPlayer.class) != null && !Verany.getPlayer(player.getUniqueId()).getPlayer(IVolcanoPlayer.class).getRound().getSettingValue(GameSetting.TAB_LIST))
+            return;
         Scoreboard scoreboard = player.getScoreboard();
-        /*for (AbstractPermissionGroup value : PermissionGroup.VALUES) {
-            if (scoreboard.getTeam(value.getScoreboardId() ) == null) {
-                Team teams = scoreboard.registerNewTeam(value.getScoreboardId());
-                teams.setPrefix(Verany.format(IngameConfig.TAB_LIST_FORMAT.getValue(), value.getColor(), value.getName()));
-                teams.setColor(value.getColor());
-                teams.setOption(Team.Option.COLLISION_RULE, IngameConfig.PLAYER_COLLISION.getValue() ? Team.OptionStatus.ALWAYS : Team.OptionStatus.NEVER);
-            }
-        }*/
-        String s = "hallo";
-        s = s.substring(s.length() - 2);
         for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
             IPermissionObject permissionObject = Verany.PROFILE_OBJECT.getPlayer(onlinePlayer.getUniqueId()).get().getPermissionObject();
             if (scoreboard.getTeam(permissionObject.getCurrentGroup().getGroup().getScoreboardId() + onlinePlayer.getUniqueId().toString().substring(0, 10)) == null) {
