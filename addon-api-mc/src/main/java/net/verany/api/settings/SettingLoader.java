023d0079eab8e7561c5def8114169e52dde88cbe
tylix <bytydoo@gmail.com>
2021-03-25 12:53:02 +0100

    Create SettingLoader.java

---
+++ addon-api-mc/src/main/java/net/verany/api/settings/SettingLoader.java
@@ -0,0 +1,30 @@
+package net.verany.api.settings;
+
+import net.verany.api.plugin.AbstractSettingLoader;
+
+import java.util.Map;
+
+public abstract class SettingLoader extends AbstractSettingLoader {
+
+    @Override
+    public <T> void setSettingValue(AbstractSetting<T> setting, T value) {
+        put(setting.getKey(), value);
+    }
+
+    @Override
+    public <T> T getSettingValue(AbstractSetting<T> setting) {
+        if (!containsKey(setting.getKey()))
+            setSettingValue(setting, setting.getDefaultValue());
+        return (T) get(setting.getKey());
+    }
+
+    @Override
+    public void loadSettings(Map<String, Object> settingMap) {
+        settingMap.forEach((s, o) -> {
+            AbstractSetting settings = Settings.getSettingByKey(s);
+            if (settings != null)
+                setSettingValue(settings, o);
+        });
+    }
+
+}
