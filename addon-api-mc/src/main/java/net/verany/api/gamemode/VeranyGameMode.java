f947e424b9b4702e772e1426fab73d23c6890796
tylix <bytydoo@gmail.com>
2021-03-26 00:28:26 +0100

    Update VeranyGameMode.java

--- addon-api-mc/src/main/java/net/verany/api/gamemode/VeranyGameMode.java
+++ addon-api-mc/src/main/java/net/verany/api/gamemode/VeranyGameMode.java
@@ -2,16 +2,8 @@ package net.verany.api.gamemode;
 
 public class VeranyGameMode {
 
-    public static final AbstractGameMode FLAG_WARS = new GameModeWrapper("FlagWars", new String[]{"FW"});
-    public static final AbstractGameMode HUB = new GameModeWrapper("Hub", new String[]{"Hub"});
-    public static final AbstractGameMode VIP_HUB = new GameModeWrapper("VIP-Hub", new String[]{"VIP-Hub"});
-    public static final AbstractGameMode ARCADE = new GameModeWrapper("Arcade", new String[]{"Bingo"});
-
-    public static AbstractGameMode getGameModeByName(String name) {
-        for (AbstractGameMode value : GameModeWrapper.VALUES)
-            if(value.getName().equalsIgnoreCase(name))
-                return value;
-        return null;
-    }
-
+    public static final AbstractGameMode FLAG_WARS = new GameModeWrapper("FlagWars", new String[]{"FW"}, "flagwars");
+    public static final AbstractGameMode HUB = new GameModeWrapper("Hub", new String[]{"Hub"}, "hubsystem");
+    public static final AbstractGameMode VIP_HUB = new GameModeWrapper("VIP-Hub", new String[]{"VIP-Hub"}, "hubsystem");
+    public static final AbstractGameMode ARCADE = new GameModeWrapper("Arcade", new String[]{"Bingo"}, "bingo");
 }
