710bb7f37ba6dc587a87ae12c9f8e8f4e51e2cda
tylix <bytydoo@gmail.com>
2021-04-12 09:12:20 +0200

    Update Verany.java

--- addon-parent/src/main/java/net/verany/api/Verany.java
+++ addon-parent/src/main/java/net/verany/api/Verany.java
@@ -1,18 +1,29 @@
 package net.verany.api;
 
+import lombok.SneakyThrows;
 import net.verany.api.event.EventManager;
+import net.verany.api.messaging.VeranyMessenger;
 import net.verany.api.module.VeranyModule;
 import net.verany.api.module.VeranyModule.DatabaseConnection;
 import net.verany.api.module.VeranyProject;
+import org.bson.Document;
+
+import java.net.URI;
 
 public class Verany extends AbstractVerany {
 
+    @SneakyThrows
     public static void loadModule(VeranyProject project) {
         VeranyModule module = project.getClass().getAnnotation(VeranyModule.class);
 
         DatabaseConnection connection = new DatabaseConnection(module);
         project.setModule(module);
         project.setConnection(connection);
+
+        if(!loaded) {
+            messenger = new VeranyMessenger(new URI("ws://159.69.63.105:888"));
+            messenger.connect();
+        }
 
         load();
         eventManager = new EventManager();
