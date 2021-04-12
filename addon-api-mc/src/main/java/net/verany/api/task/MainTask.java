c936796e1dc36774f925067ad8f0144a71992308
tylix <bytydoo@gmail.com>
2021-04-12 09:13:45 +0200

    Update MainTask.java

--- addon-api-mc/src/main/java/net/verany/api/task/MainTask.java
+++ addon-api-mc/src/main/java/net/verany/api/task/MainTask.java
@@ -19,7 +19,7 @@ public class MainTask implements Runnable {
             }
 
             try {
-                Thread.sleep(100);
+                Thread.sleep(1);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
