81573b8bff83a4d0dd26a6d1215870408b870166
tylix <bytydoo@gmail.com>
2021-04-12 09:14:05 +0200

    Create MessageInEvent.java

---
+++ addon-api-mc/src/main/java/net/verany/api/event/events/MessageInEvent.java
@@ -0,0 +1,26 @@
+package net.verany.api.event.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.verany.api.event.VeranyEvent;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.json.JSONObject;
+
+@AllArgsConstructor
+@Getter
+public class MessageInEvent extends Event {
+
+    private final JSONObject message;
+
+    private static final HandlerList handlers = new HandlerList();
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    public HandlerList getHandlers() {
+        return getHandlerList();
+    }
+
+}
