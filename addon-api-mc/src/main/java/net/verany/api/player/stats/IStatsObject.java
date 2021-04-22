e9d6816e3fe4ba774644bbf94dd80fc0a7bfe220
tylix <bytydoo@gmail.com>
2021-04-22 22:17:57 +0200

    Update IStatsObject.java

--- addon-api-mc/src/main/java/net/verany/api/player/stats/IStatsObject.java
+++ addon-api-mc/src/main/java/net/verany/api/player/stats/IStatsObject.java
@@ -2,6 +2,8 @@ package net.verany.api.player.stats;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
 import net.verany.api.interfaces.IDefault;
 import net.verany.api.module.VeranyProject;
 import org.bukkit.Bukkit;
@@ -15,9 +17,13 @@ public interface IStatsObject extends IDefault<UUID> {
 
     <T> List<T> getStatsData(AbstractStatsType<T> statsType, StatsTime statsTime);
 
+    <T> List<T> getStatsData(AbstractStatsType<T> statsType, long date);
+
     <T> void setStatsData(AbstractStatsType<T> statsData, T value);
 
     int getStatsValue(AbstractStatsType<Integer> statsData, StatsTime time);
+
+    int getStatsValue(AbstractStatsType<Integer> statsData, long date);
 
     String getKd(int kills, int deaths);
 
@@ -27,13 +33,22 @@ public interface IStatsObject extends IDefault<UUID> {
 
     @AllArgsConstructor
     @Getter
-    enum StatsTime {
+    enum StatsTime implements Cloneable {
         ALL_TIME(Long.MAX_VALUE),
         MONTHLY(TimeUnit.DAYS.toMillis(30)),
         WEEKLY(TimeUnit.DAYS.toMillis(7)),
         DAILY(TimeUnit.DAYS.toMillis(1));
 
-        private final long time;
+        @Setter
+        private long time;
+
+        @SneakyThrows
+        public StatsTime of(int amount) {
+            StatsTime toReturn = (StatsTime) clone();
+            long currentTime = toReturn.getTime();
+            toReturn.setTime(currentTime * amount);
+            return toReturn;
+        }
     }
 
 
