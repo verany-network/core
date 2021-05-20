ff31f0ddfb929d54c24719daec6221541a11a20c
tylix <bytydoo@gmail.com>
2021-05-20 22:39:57 +0200

    Create UpdateEntry.java

---
+++ addon-api-mc/src/main/java/net/verany/api/update/date/UpdateEntry.java
@@ -0,0 +1,26 @@
+package net.verany.api.update.date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.verany.api.AbstractVerany;
+
+import java.util.Deque;
+import java.util.UUID;
+
+@AllArgsConstructor
+@Getter
+public class UpdateEntry {
+
+    private final String id = AbstractVerany.generate(10);
+    private final String author;
+    private final String topic;
+    private final Deque<String> pages;
+    private final UUID creator;
+    private final long timestamp = System.currentTimeMillis();
+    private final long publishDate;
+
+    public boolean isPublished() {
+        return System.currentTimeMillis() >= publishDate;
+    }
+
+}
