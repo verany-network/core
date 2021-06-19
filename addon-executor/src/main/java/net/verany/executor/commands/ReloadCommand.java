65878e4a3e4fde0f89f28b1961ff7b76a60af571
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-06-19 02:04:34 +0200

    Adjustments to the message designs
    [+] Fixed prefix
    [+] Fixed special characters

--- addon-executor/src/main/java/net/verany/executor/commands/ReloadCommand.java
+++ addon-executor/src/main/java/net/verany/executor/commands/ReloadCommand.java
@@ -29,124 +29,124 @@ public class ReloadCommand implements CommandExecutor {
                 if (args[0].equalsIgnoreCase("messages")) {
                     reloadType = VeranyReloadEvent.ReloadType.MESSAGES;
                     try {
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading messages§8...");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading messages§8...");
                         Verany.reloadMessages(CoreExecutor.INSTANCE);
                     } catch (Exception e) {
                         e.printStackTrace();
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§4An error occurred while reloading messages! [" + e + "]");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§4An error occurred while reloading messages! [" + e + "]");
                         success = false;
                     } finally {
                         if (success)
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§aReloading " + Verany.asDecimal(Verany.MESSAGES.size()) + " messages success!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§aReloading " + Verany.asDecimal(Verany.MESSAGES.size()) + " messages success!");
                         else
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cReloading messages failed!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cReloading messages failed!");
                     }
                 } else if (args[0].equalsIgnoreCase("setup")) {
                     reloadType = VeranyReloadEvent.ReloadType.USER;
                     try {
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading setup§8...");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading setup§8...");
                         Verany.reloadSetup();
                     } catch (Exception e) {
                         e.printStackTrace();
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§4An error occurred while reloading setup! [" + e + "]");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§4An error occurred while reloading setup! [" + e + "]");
                         success = false;
                     } finally {
                         if (success)
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§aReloading setup success!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§aReloading setup success!");
                         else
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cReloading setup failed!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cReloading setup failed!");
                     }
                 } else if (args[0].equalsIgnoreCase("users")) {
                     reloadType = VeranyReloadEvent.ReloadType.USER;
                     try {
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading users§8...");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading users§8...");
                         //Verany.reloadUser();
                     } catch (Exception e) {
                         e.printStackTrace();
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§4An error occurred while reloading users! [" + e + "]");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§4An error occurred while reloading users! [" + e + "]");
                         success = false;
                     } finally {
                         if (success)
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§aReloading users success!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§aReloading users success!");
                         else
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cReloading users failed!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cReloading users failed!");
                     }
                 } else if (args[0].equalsIgnoreCase("permissions")) {
                     reloadType = VeranyReloadEvent.ReloadType.PERMISSIONS;
                     try {
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading permissions§8...");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading permissions§8...");
                         Verany.loadPermissionGroups(CoreExecutor.INSTANCE);
                         Bukkit.getOnlinePlayers().forEach(onlinePlayer -> new TabListObject().setTabList(onlinePlayer));
                     } catch (Exception e) {
                         e.printStackTrace();
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§4An error occurred while reloading permissions! [" + e + "]");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§4An error occurred while reloading permissions! [" + e + "]");
                         success = false;
                     } finally {
                         if (success)
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§aReloading permissions success!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§aReloading permissions success!");
                         else
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cReloading permissions failed!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cReloading permissions failed!");
                     }
                 } else if (args[0].equalsIgnoreCase("plugins")) {
                     reloadType = VeranyReloadEvent.ReloadType.PLUGINS;
                     try {
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Disabling plugins§8...");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Disabling plugins§8...");
                         for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
                             if (!plugin.getName().contains("CloudNetAPI"))
                                 Bukkit.getPluginManager().disablePlugin(plugin);
                         }
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Load plugins§8...");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Load plugins§8...");
                         File pluginFolder = new File("plugins");
                         for (Plugin plugin : Bukkit.getPluginManager().loadPlugins(pluginFolder)) {
                             Bukkit.getPluginManager().enablePlugin(plugin);
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Enabling plugin§8: §b" + plugin.getName() + "§8...");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Enabling plugin§8: §b" + plugin.getName() + "§8...");
                         }
                     } catch (Exception e) {
                         e.printStackTrace();
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§4An error occurred while reloading plugins! [" + e + "]");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§4An error occurred while reloading plugins! [" + e + "]");
                         success = false;
                     } finally {
                         if (success)
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§aReloading plugins success!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§aReloading plugins success!");
                         else
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cReloading plugins failed!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cReloading plugins failed!");
                     }
                 } else if (args[0].equalsIgnoreCase("proxy")) {
                     reloadType = VeranyReloadEvent.ReloadType.PROXY_ALL;
-                    sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading proxy§8...");
+                    sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading proxy§8...");
 
                 } else if (args[0].equalsIgnoreCase("file")) {
-                    sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cSyntax: /reload file <file-name>");
+                    sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cSyntax: /reload file <file-name>");
                     sender.sendMessage(" ");
                 } else if (args[0].equalsIgnoreCase("bukkit")) {
                     reloadType = VeranyReloadEvent.ReloadType.BUKKIT;
                     try {
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading Bukkit§8...");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading Bukkit§8...");
                         Bukkit.reload();
                     } catch (Exception e) {
                         e.printStackTrace();
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§4An error occurred while reloading Bukkit! [" + e + "]");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§4An error occurred while reloading Bukkit! [" + e + "]");
                         success = false;
                     } finally {
                         if (success)
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§aReloading Bukkit success!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§aReloading Bukkit success!");
                         else
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cReloading Bukkit failed!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cReloading Bukkit failed!");
                     }
                 } else if (args[0].equalsIgnoreCase("locations")) {
                     reloadType = VeranyReloadEvent.ReloadType.LOCATIONS;
                     try {
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading locations§8...");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading locations§8...");
                         //Verany.loadLocations();
                     } catch (Exception e) {
                         e.printStackTrace();
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§4An error occurred while reloading locations! [" + e + "]");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§4An error occurred while reloading locations! [" + e + "]");
                         success = false;
                     } finally {
                         if (success)
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§aReloading locations success!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§aReloading locations success!");
                         else
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cReloading locations failed!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cReloading locations failed!");
                     }
                 } else {
                     sendHelp(sender);
@@ -157,21 +157,21 @@ public class ReloadCommand implements CommandExecutor {
                 if (args[0].equalsIgnoreCase("file")) {
                     reloadType = VeranyReloadEvent.ReloadType.FILE;
                     file = args[1];
-                    sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading file§8: §b" + file + "§8...");
+                    sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading file§8: §b" + file + "§8...");
                 } else if (args[0].equalsIgnoreCase("proxy")) {
                     switch (args[1]) {
                         case "messages":
                             reloadType = VeranyReloadEvent.ReloadType.PROXY_MESSAGES;
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading proxy messages§8...");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading proxy messages§8...");
                             break;
                         case "users":
                             reloadType = VeranyReloadEvent.ReloadType.PROXY_USERS;
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading proxy users§8...");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading proxy users§8...");
 
                             break;
                         case "permissions":
                             reloadType = VeranyReloadEvent.ReloadType.PROXY_PERMISSIONS;
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reloading proxy permissions§8...");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reloading proxy permissions§8...");
 
                             break;
                     }
@@ -185,13 +185,13 @@ public class ReloadCommand implements CommandExecutor {
                         Bukkit.getPluginManager().enablePlugin(newPlugin);
                     } catch (Exception e) {
                         e.printStackTrace();
-                        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§4An error occurred while reloading plugin (" + pluginName + ")! [" + e + "]");
+                        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§4An error occurred while reloading plugin (" + pluginName + ")! [" + e + "]");
                         success = false;
                     } finally {
                         if (success)
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§aReloading plugin (" + pluginName + ") success!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§aReloading plugin (" + pluginName + ") success!");
                         else
-                            sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§cReloading plugin (" + pluginName + ") failed!");
+                            sender.sendMessage(Verany.getPrefix("VeranyCore") + "§cReloading plugin (" + pluginName + ") failed!");
                     }
                 } else {
                     sendHelp(sender);
@@ -210,20 +210,20 @@ public class ReloadCommand implements CommandExecutor {
 
     private void sendHelp(CommandSender sender) {
         sender.sendMessage(" ");
-        sender.sendMessage(Verany.getPrefix("VeranySpigot") + "§7Reload commands§8:");
+        sender.sendMessage(Verany.getPrefix("VeranyCore") + "§7Reload commands§8:");
         sender.sendMessage(" ");
-        sender.sendMessage(" §8» §b/reload bukkit");
-        sender.sendMessage(" §8» §b/reload messages");
-        sender.sendMessage(" §8» §b/reload users");
-        sender.sendMessage(" §8» §b/reload worlds");
-        sender.sendMessage(" §8» §b/reload permissions");
+        sender.sendMessage(" §8▬§7▬ §b/reload bukkit");
+        sender.sendMessage(" §8▬§7▬ §b/reload messages");
+        sender.sendMessage(" §8▬§7▬ §b/reload users");
+        sender.sendMessage(" §8▬§7▬ §b/reload worlds");
+        sender.sendMessage(" §8▬§7▬ §b/reload permissions");
         //sender.sendMessage(" §8» §b/reload plugins (<name>)");
-        sender.sendMessage(" §8» §b/reload settings");
-        sender.sendMessage(" §8» §b/reload achievements");
-        sender.sendMessage(" §8» §b/reload manager");
-        sender.sendMessage(" §8» §b/reload locations");
-        sender.sendMessage(" §8» §b/reload proxy (<messages, users, permissions>)");
-        sender.sendMessage(" §8» §b/reload file <file-name>");
+        sender.sendMessage(" §8▬§7▬ §b/reload settings");
+        sender.sendMessage(" §8▬§7▬ §b/reload achievements");
+        sender.sendMessage(" §8▬§7▬ §b/reload manager");
+        sender.sendMessage(" §8▬§7▬ §b/reload locations");
+        sender.sendMessage(" §8▬§7▬ §b/reload proxy (<messages, users, permissions>)");
+        sender.sendMessage(" §8▬§7▬ §b/reload file <file-name>");
         sender.sendMessage(" ");
     }
 
